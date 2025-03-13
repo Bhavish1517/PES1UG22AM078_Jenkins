5d67e06543d8e8445b2171117745740e238531f5
K Bhavish Raju <134196391+Bhavish1517@users.noreply.github.com>
2025-03-13 23:04:14 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello\n";
+  cout << "Testing the Pipeline in Jenkins\n";
+  cout << "Pipeline is working succesfully !!!  \n";
+  cout << "This above is the output of the main.cpp file\n";
+  return 0;
+}
