085ee01cc35c5ec77fe6931fa291ea26df1d576b
K Bhavish Raju <134196391+Bhavish1517@users.noreply.github.com>
2025-03-13 21:45:51 +0530

    Update hello.cpp to check Webhooks

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Making a change to check working of webhooks\n";
+  cout << "Making a change to check working of Webhooks\n";
+  cout << "Webhooks working !!!\n";
   return 0;
 }
